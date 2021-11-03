056b369728a318a444ace8510cedabe4f2d8ea63
Ivan C <chebiva@gmail.com>
2021-11-03 13:56:37 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
 			<Html lang="en-US">
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap"
 						rel="stylesheet"
