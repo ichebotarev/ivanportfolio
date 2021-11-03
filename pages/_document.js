34be86efe402a5a08b97e2840aa2621c759a173c
Ivan C <chebiva@gmail.com>
2021-11-03 14:20:00 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,52 @@
-import Document, { Html, Head, NextScript, Main } from "next/document";
+import React from "react";
+import Document, {
+	DocumentContext,
+	DocumentInitialProps,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-	static getInitialProps({ renderPage }) {
+	static async getInitialProps(ctx) {
 		const sheet = new ServerStyleSheet();
-		const page = renderPage(
-			(App) => (props) => sheet.collectStyles(<App {...props} />)
-		);
-		const styleTags = sheet.getStyleElement();
-		return { ...page, styleTags };
-	}
+		const originalRenderPage = ctx.renderPage;
 
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
 	render() {
 		return (
-			<Html lang="en-CA">
+			<Html lang="en">
 				<Head>
+					<title>Ivan Chebotarev</title>
+					<meta charSet="utf-8" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap"
 						rel="stylesheet"
-					/>{" "}
+					/>
 				</Head>
 				<body>
 					<Main />
